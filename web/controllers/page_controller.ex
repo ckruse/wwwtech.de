1b96098250293d99e3eb6a42e6d3710636f4e486
Christian Kruse <cjk@defunct.ch>
2015-12-26 15:53:58 +0100

    feat: now we have a combined feed at the front

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,8 +1,28 @@
 defmodule Wwwtech.PageController do
   use Wwwtech.Web, :controller
 
+  alias Wwwtech.Note
+  alias Wwwtech.Picture
+  alias Wwwtech.Article
+
   def index(conn, _params) do
-    render conn, "index.html"
+    article = Article |> Article.with_author |> Article.sorted |> Article.last_x(1) |> Repo.one
+    entries = ((Note |> Note.with_author |> Note.sorted |> Note.last_x(10) |> Repo.all) ++
+      (Picture |> Picture.with_author |> Picture.sorted |> Picture.last_x(10) |> Repo.all)) |>
+      Enum.sort(&(Timex.Date.compare(Note.inserted_at_timex(&1), Note.inserted_at_timex(&2)) == 1)) |> Enum.slice(0, 10)
+
+    {entries_by_day, keys} = Enum.reduce entries, {%{}, []}, fn item, {nbd, keys} ->
+      {date, _} = Ecto.DateTime.to_erl(item.inserted_at)
+      if nbd[date] == nil do
+        {Map.put(nbd, date, [item]), keys ++ [date]}
+      else
+        {Map.put(nbd, date, nbd[date] ++ [item]), keys}
+      end
+    end
+
+    render(conn, "index.html", entries: entries,
+           entries_by_day: entries_by_day, days: keys,
+           article: article)
   end
 
   def about(conn, _params) do
