0bf4dde4b502c18e4d1db7fb4c5c1061814b2558
Christian Kruse <cjk@defunct.ch>
2016-02-01 13:47:17 +0100

    fix: typo

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :wwwtech, Wwwtech.Endpoint,
   http: [port: 4000],
   url: [scheme: "https", host: "wwwtech.de", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
+  cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
