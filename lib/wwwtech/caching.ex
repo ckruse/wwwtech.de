9c3394aaf1b2c6d8269f56c87331841843924486
Christian Kruse <cjk@defunct.ch>
2016-02-02 13:30:56 +0100

    fix: remove debugging comment

--- lib/wwwtech/caching.ex
+++ lib/wwwtech/caching.ex
@@ -2,7 +2,7 @@ defmodule Wwwtech.CachingPlug do
   import Plug.Conn
 
   def set_caching_headers(conn, _opts \\ []) do
-    if not Wwwtech.Session.logged_in?(conn) do # and Mix.env == "prod" do
+    if not Wwwtech.Session.logged_in?(conn) and Mix.env == "prod" do
       cache_time = Timex.Date.now |> Timex.Date.add(Timex.Time.to_timestamp(1, :hours))
       conn |>
         put_resp_header("expires", cache_time |> Timex.DateFormat.format!("{RFC1123}")) |>
