c69f76e5e48b9c398ed8600b8fead5424eac1569
Christian Kruse <cjk@defunct.ch>
2016-02-03 11:06:13 +0100

    fix: typo

--- priv/repo/migrations/20151220194925_create_author.exs
+++ priv/repo/migrations/20151220194925_create_author.exs
@@ -6,7 +6,7 @@ defmodule Wwwtech.Repo.Migrations.CreateAuthor do
       add :name, :string, null: false
       add :email, :string, null: false
       add :avatar, :string, null: false
-      add :encrypted_password, :string, null: false, default: ''
+      add :encrypted_password, :string, null: false, default: ""
 
       timestamps
     end
