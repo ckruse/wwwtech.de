510d4a55b345ad174fef829101ca065238b00438
Christian Kruse <cjk@defunct.ch>
2017-01-19 11:30:43 +0100

    ref: explicitely linking to profile page in navigation

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Wwwtech.Mixfile do
 
   def project do
     [app: :wwwtech,
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
