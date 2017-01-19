62b77810ccbcb8deae83c127177c64c9a2979b91
Christian Kruse <cjk@defunct.ch>
2017-01-19 11:05:41 +0100

    fix: typo

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Wwwtech.Mixfile do
 
   def project do
     [app: :wwwtech,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
