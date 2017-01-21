d4002632fe374cd2ecbf153158b3711f145eac08
Christian Kruse <cjk@defunct.ch>
2017-01-21 11:20:58 +0100

    version upgrade

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Wwwtech.Mixfile do
 
   def project do
     [app: :wwwtech,
-     version: "0.0.9",
+     version: "0.0.10",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
