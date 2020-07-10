1a04767a1babac0891accbac71fad89f29ec2985
Christian Kruse <cjk@defunct.ch>
2020-07-10 19:56:23 +0200

    refactoring: move webmention validation to its own module

--- lib/wwwtech_web/controllers/webmention_controller.ex
+++ lib/wwwtech_web/controllers/webmention_controller.ex
@@ -8,9 +8,10 @@ defmodule WwwtechWeb.WebmentionController do
 
   alias Wwwtech.Mentions
   alias Wwwtech.Mentions.Mention
+  alias Wwwtech.Mentions.Helpers
 
   def create(conn, params) do
-    case validate_mention(conn, params) do
+    case Helpers.validate_mention(conn, params) do
       true -> save_mention(conn, params)
       {:error, nconn} -> nconn
     end
@@ -26,7 +27,7 @@ defmodule WwwtechWeb.WebmentionController do
     if blank?(object) && present?(type) do
       conn |> send_resp(400, "This is not the host you're looking for (object could not be found)")
     else
-      parsed = values_from_remote(params["source"])
+      parsed = Helpers.values_from_remote(params["source"])
 
       old_mention =
         case Mentions.get_mention_by_source_and_target(parsed["url"], params["target"]) do
@@ -68,32 +69,6 @@ defmodule WwwtechWeb.WebmentionController do
     end
   end
 
-  def validate_mention(conn, params) do
-    cond do
-      String.trim(to_string(params["target"])) == "" ->
-        {:error, conn |> send_resp(400, "This is not the host you're looking for (target is blank)")}
-
-      String.trim(to_string(params["source"])) == "" ->
-        {:error, conn |> send_resp(400, "This is not the host you're looking for (source is blank)")}
-
-      !valid_target?(conn, params["target"]) ->
-        {:error, conn |> send_resp(400, "This is not the host you're looking for (host is not equal to my host)")}
-
-      Webmentions.is_valid_mention(params["source"], params["target"]) ->
-        true
-
-      true ->
-        {:error, conn |> send_resp(400, "This is not the host you're looking for (mention is not valid)")}
-    end
-  end
-
-  defp valid_target?(conn, target) do
-    target_uri = URI.parse(target)
-    my_uri = URI.parse(Routes.page_url(conn, :index))
-
-    target_uri.host == my_uri.host
-  end
-
   defp object_id_with_key(attributes, "notes", object), do: Map.put(attributes, :note_id, object.id)
   defp object_id_with_key(attributes, "pictures", object), do: Map.put(attributes, :picture_id, object.id)
   defp object_id_with_key(attributes, "articles", object), do: Map.put(attributes, :article_id, object.id)
@@ -105,108 +80,4 @@ defmodule WwwtechWeb.WebmentionController do
   defp object_by_module("pictures", id), do: Pictures.get_picture!(id)
   defp object_by_module("likes", id), do: Likes.get_like!(id)
   defp object_by_module(_, _), do: nil
-
-  defp values_from_remote(url) do
-    response =
-      [Tesla.Middleware.FollowRedirects]
-      |> Tesla.client()
-      |> Tesla.get(url)
-
-    with {:ok, %Tesla.Env{status: code} = response} when code in 200..299 <- response,
-         %{"items" => items} = mf when items != [] <- Microformats2.parse(response.body, url) do
-      item = List.first(mf["items"])
-
-      %{
-        "author" => get_value(item, "author"),
-        "author_url" => get_sub_key(item, "author", "url"),
-        "author_avatar" => get_sub_key(item, "author", "photo"),
-        "title" => get_value(item, "name"),
-        "excerpt" => get_excerpt(item),
-        "mention_type" => guess_mention_type(item),
-        "url" => get_url(url, mf)
-      }
-    else
-      # we got a successful request but no microformats
-      {:ok, %Tesla.Env{status: code} = response} when code in 200..299 -> get_values_from_html(url, response.body)
-      _ -> %{}
-    end
-  end
-
-  def get_url(url, mf) do
-    if Regex.match?(~r/brid-gy.appspot.com/, url),
-      do: List.first(mf["items"])["properties"]["url"] |> List.first(),
-      else: url
-  end
-
-  defp get_value(item, key) do
-    kv = (item["properties"][key] || []) |> List.first()
-
-    cond do
-      kv == nil -> nil
-      is_bitstring(kv) -> kv
-      true -> kv["value"]
-    end
-  end
-
-  defp get_sub_key(item, key, subkey) do
-    key_val = (item["properties"][key] || []) |> List.first()
-
-    cond do
-      is_map(key_val) and is_map(key_val["properties"]) ->
-        (key_val["properties"][subkey] || []) |> List.first()
-
-      true ->
-        nil
-    end
-  end
-
-  defp get_excerpt(item) do
-    excerpt = (item["properties"]["content"] || []) |> List.first()
-
-    cond do
-      excerpt != nil and is_map(excerpt) -> excerpt["text"]
-      excerpt != nil and is_bitstring(excerpt) -> excerpt
-      excerpt != nil -> raise inspect(excerpt)
-      true -> nil
-    end
-  end
-
-  defp guess_mention_type(item) do
-    cond do
-      present?(get_value(item, "in-reply-to")) -> "reply"
-      present?(get_value(item, "like-of")) -> "like"
-      present?(get_value(item, "repost-of")) -> "repost"
-      persontag?(item) -> "persontag"
-      true -> nil
-    end
-  end
-
-  defp persontag?(item) do
-    present?(item["properties"]["category"]) &&
-      (List.first(item["properties"]["category"])["type"] || []) |> List.first() == "h-card"
-  end
-
-  defp get_values_from_html(url, html) do
-    case Floki.parse_document(html) do
-      {:ok, doc} ->
-        author = Floki.find(doc, "meta[name=author]") |> Floki.attribute("content") |> List.first()
-        excerpt = Floki.find(doc, "meta[name=description]") |> Floki.attribute("content") |> List.first()
-        title = Floki.find(doc, "meta[property='og:title']") |> Floki.attribute("content") |> List.first()
-        author_url = Floki.find(doc, "meta[property='og:article:author']") |> Floki.attribute("content") |> List.first()
-        author_avatar = Floki.find(doc, "meta[property='og:image']") |> Floki.attribute("content") |> List.first()
-
-        %{
-          "author" => author,
-          "author_url" => author_url,
-          "author_avatar" => author_avatar,
-          "title" => title,
-          "excerpt" => excerpt,
-          "mention_type" => "reply",
-          "url" => url
-        }
-
-      _ ->
-        %{}
-    end
-  end
 end
