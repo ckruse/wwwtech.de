72b97a4772fb1345bc581bb585269faaa2407800
Christian Kruse <cjk@defunct.ch>
2020-12-16 20:32:34 +0100

    ref: start building on github actions

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,11 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :wwwtech, WwwtechWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  root: ".",
+  version: Mix.Project.config()[:version]
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -52,6 +55,3 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "prod.secret.exs"
-
-config :appsignal, :config, active: true
---
+++ config/releases.exs
@@ -0,0 +1,38 @@
+import Config
+
+config :wwwtech, WwwtechWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: System.fetch_env!("WWWTECH_PORT")],
+  url: [scheme: "https", host: "wwwtech.de", port: 443],
+  secret_key_base: System.fetch_env!("WWWTECH_SECRET_KEY")
+
+config :wwwtech, storage_path: System.fetch_env!("WWWTECH_STORAGE_PATH")
+config :wwwtech, cache_path: System.fetch_env!("WWWTECH_CACHE_PATH")
+config :wwwtech, :environment, :prod
+config :wwwtech, :keybase, System.fetch_env!("WWWTECH_KEYBASE")
+
+config :wwwtech, Wwwtech.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.fetch_env!("WWWTECH_DB_USER"),
+  password: System.fetch_env!("WWWTECH_DB_PASSWORD"),
+  database: System.fetch_env!("WWWTECH_DB_NAME"),
+  pool_size: 20
+
+config :wwwtech, Wwwtech.Mailer,
+  adapter: Swoosh.Adapters.SMTP,
+  relay: System.fetch_env!("WWWTECH_SMTP_SERVER"),
+  port: System.fetch_env!("WWWTECH_SMTP_PORT") |> String.to_integer(),
+  username: System.fetch_env!("WWWTECH_SMTP_USER"),
+  password: System.fetch_env!("WWWTECH_SMTP_PASSWORD"),
+  tls: :if_available,
+  auth: :always,
+  retries: 3
+
+config :appsignal, :config,
+  name: "WWWTech",
+  push_api_key: System.fetch_env!("WWWTECH_APPSIGNAL_KEY"),
+  env: Mix.env(),
+  active: true,
+  otp_app: :wwwtech,
+  ecto_repos: [],
+  working_directory_path: "/tmp/wwwtech",
+  log_path: "/tmp/wwwtech/"
