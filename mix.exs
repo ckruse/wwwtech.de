a6327bf2f3755730911cf5bc8ac9d4b7e3d951cd
Christian Kruse <cjk@defunct.ch>
2021-02-20 11:46:14 +0100

    require at least Elixir 1.7

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Wwwtech.MixProject do
     [
       app: :wwwtech,
       version: "0.10.16",
-      elixir: "~> 1.5",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
