788d09794f0eb53ec34b751b0bb8740527bb0ca3
Christian Kruse <cjk@defunct.ch>
2021-05-15 07:48:54 +0200

    version 0.11.19

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Wwwtech.MixProject do
   def project do
     [
       app: :wwwtech,
-      version: "0.11.18",
+      version: "0.11.19",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
