1e5beb5ee6e0ca931dff801f45e266e2d286a223
Christian Kruse <cjk@defunct.ch>
2021-12-03 21:56:46 +0100

    bump required elixir version to 1.12

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Wwwtech.MixProject do
     [
       app: :wwwtech,
       version: "0.12.10",
-      elixir: "~> 1.7",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
