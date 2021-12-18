3d381f5007b705a8c7601dcff5d83248927ac339
Christian Kruse <cjk@defunct.ch>
2021-12-18 21:55:38 +0100

    robots.txt & humans.txt

--- src/static_handlers.rs
+++ src/static_handlers.rs
@@ -12,10 +12,26 @@ pub async fn favicon() -> Result<fs::NamedFile> {
     Ok(fs::NamedFile::open(path)?)
 }
 
+#[get("/robots.txt")]
+pub async fn robots_txt() -> Result<fs::NamedFile> {
+    let mut path = utils::static_path();
+    path.push_str("/robots.txt");
+
+    Ok(fs::NamedFile::open(path)?)
+}
+
 #[get("/A99A9D73.asc")]
 pub async fn gpgkey() -> Result<fs::NamedFile> {
     let mut path = utils::static_path();
     path.push_str("/A99A9D73.asc");
+
+    Ok(fs::NamedFile::open(path)?)
+}
+
+#[get("/humans.txt")]
+pub async fn humans_txt() -> Result<fs::NamedFile> {
+    let mut path = utils::static_path();
+    path.push_str("/humans.txt");
 
     Ok(fs::NamedFile::open(path)?)
 }
