e2f6e8552f6804770e85faea35953d70c6949c80
Christian Kruse <cjk@defunct.ch>
2024-11-10 09:25:44 +0100

    change content_warning to text instead of varchar(255)

---
+++ migrations/20241110081708_change_content_warning_to_text.up.sql
@@ -0,0 +1,8 @@
+ALTER TABLE articles
+  ALTER COLUMN content_warning TYPE TEXT;
+ALTER TABLE notes
+  ALTER COLUMN content_warning TYPE TEXT;
+ALTER TABLE pictures
+  ALTER COLUMN content_warning TYPE TEXT;
+ALTER TABLE deafies
+  ALTER COLUMN content_warning TYPE TEXT;
